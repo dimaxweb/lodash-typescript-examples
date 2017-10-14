2b36c0a0b8cb15723dc863791ea4d6e23a67aebf
Dmitry Mogilco <dmogilc@ecitele.com>
2017-10-14 23:40:18 +0300

    add partial examples

--- __tests__/function/partial.spec.js
+++ __tests__/function/partial.spec.js
@@ -9,3 +9,32 @@ test('Partial function', function () {
     var greeting = sayHelloTo('fred');
     expect(greeting).toEqual('hello fred');
 });
+function propertyValidate(func, path, data) {
+    var property = lodash_1.get(data, path);
+    return func(property);
+}
+var utils = {
+    greaterThan: lodash_1.curryRight(function (lhs, rhs) { return lhs > rhs; }),
+    lessThan: lodash_1.curryRight(function (lhs, rhs) { return lhs < rhs; })
+};
+var priceLessThan10 = lodash_1.partial(propertyValidate, utils.lessThan(10), 'info.price');
+test('price is less than 10', function () {
+    var data = {
+        info: {
+            name: 'Phone',
+            price: 8,
+        }
+    };
+    expect(priceLessThan10(data)).toBeTruthy();
+});
+var maxPriceGreaterThan = lodash_1.partial(propertyValidate, utils.greaterThan(250), 'info.maxPrice');
+test('max price is greate than 250', function () {
+    var data = {
+        info: {
+            name: 'Phone',
+            price: 8,
+            maxPrice: 360
+        }
+    };
+    expect(maxPriceGreaterThan(data)).toBeTruthy();
+});
--- __tests__/function/partial.spec.ts
+++ __tests__/function/partial.spec.ts
@@ -1,15 +1,54 @@
-import {partial} from 'lodash'
+import {partial,get,curry,curryRight} from 'lodash'
 
 function greet(greeting, name) {
   return greeting + ' ' + name;
 }
+
 
 test('Partial function',()=>{
   const sayHelloTo = partial(greet, 'hello');
   var greeting  = sayHelloTo('fred');
   expect(greeting).toEqual('hello fred')
 
+});
+
+function propertyValidate(func :Function,path : string,data : any){
+  var property = get(data,path);
+  return func(property);
+}
+
+const utils  = {
+  greaterThan : curryRight(function (lhs:any, rhs:any) { return lhs > rhs }),
+  lessThan    : curryRight(function (lhs:any, rhs:any) { return lhs < rhs })
+}
+
+var priceLessThan10  = partial(propertyValidate,utils.lessThan(10),'info.price');
+
+test('price is less than 10',()=>{
+  var data  = {
+    info :{
+     name  : 'Phone',
+     price : 8,
+    }
+  };
+
+  expect(priceLessThan10(data)).toBeTruthy();
+
 })
+
+
+var maxPriceGreaterThan  = partial(propertyValidate,utils.greaterThan(250),'info.maxPrice');
+test('max price is greate than 250',()=>{
+  var data  = {
+    info :{
+      name  : 'Phone',
+      price : 8,
+      maxPrice : 360
+    }
+  };
+  expect(maxPriceGreaterThan(data)).toBeTruthy();
+});
+
 
 
 
